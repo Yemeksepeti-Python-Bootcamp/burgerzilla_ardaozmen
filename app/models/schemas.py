4f33f14858f56a358b60ad91c133f7c015d0ff7a
Arda Cem Özmen <ardaozmen3@gmail.com>
2022-02-11 22:57:17 +0300

    User model created, auth process added

---
+++ app/models/schemas.py
@@ -0,0 +1,7 @@
+from app import ma
+from .user import User
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("email", "name", "username", "joined_date", "role_id")
