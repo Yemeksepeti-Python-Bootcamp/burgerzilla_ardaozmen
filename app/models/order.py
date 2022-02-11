f4d55b5f49b14d117fdfe82c127ec05ef7e032b1
Arda Cem Özmen <ardaozmen3@gmail.com>
2022-02-12 01:25:23 +0300

    Order model added

---
+++ app/models/order.py
@@ -0,0 +1,18 @@
+import enum
+
+from app import db
+
+class OrderStatus(enum.Enum):
+    new = "Yeni"
+    preparing = "Hazirlaniyor"
+    on_the_way = "Yolda"
+    delivery = "Teslim"
+    cancel = "Iptal"
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    restaurant_id = db.Column(db.Integer)
+    status = db.Column(db.Enum(OrderStatus))
+    # TODO this model will be updated
