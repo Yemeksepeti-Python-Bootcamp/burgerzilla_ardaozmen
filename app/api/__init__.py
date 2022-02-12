d99dc39483a4d3d70dbf23b1ba7e8e20c45b58a5
Arda Cem Özmen <ardaozmen3@gmail.com>
2022-02-12 20:12:44 +0300

    Blueprint namespaces added

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 
 from .user.controller import api as user_ns
 from .restaurant.controller import api as restaurant_ns
+from .order.controller import api as order_ns
+from .menu.controller import api as menu_ns
 
 api_bp = Blueprint("api", __name__)
 api = Api(api_bp, version="1.", title="API", description="API")
@@ -10,3 +12,5 @@ api = Api(api_bp, version="1.", title="API", description="API")
 
 api.add_namespace(user_ns)
 api.add_namespace(restaurant_ns)
+api.add_namespace(order_ns)
+api.add_namespace(menu_ns)
